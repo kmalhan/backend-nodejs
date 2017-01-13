178eb1247cf9c5b2e8ee83874145fe93819219fa
Kazumi Malhan <kazumimalhan@gmail.com>
2017-01-12 23:00:30 -0500

    access computer information using nodejs

---
+++ getOSInfo.js
@@ -0,0 +1,13 @@
+var os = require("os");
+
+console.log("\nTemporary directory .. " + os.tmpdir());
+console.log("\nEndianness ........... " + 
+	os.endianness() == "LE" ? "Low-endian" : "High-endian");
+console.log("\nHost name ............ " + os.hostname());
+console.log("\nOS type .............. " + os.type());
+console.log("\nOS release ........... " + os.release());
+console.log("\nTotal system memory .. " + os.totalmem());
+console.log("\nCPU information ...... " + JSON.stringify(os.cpus()));
+console.log("\nNIC information ...... " + 
+	JSON.stringify(os.networkInterfaces())
+	);
